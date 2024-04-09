cc123fbb8b03fc03e7cf9e76eca41d1710a349cb
AxelNAY <axelnay@sfr.fr>
2024-04-09 07:45:28 +0000

    destroy the precedent change

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,12 +10,9 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if (new == NULL)
+	if (parent == NULL || new == NULL)
 		return (NULL);
 
 	new->n = value;
