4ddae8c2fe0b99d91b0970b443e86708d75795c2
AxelNAY <axelnay@sfr.fr>
2024-04-09 07:14:10 +0000

    Destroy the condition parent == NULL in the if of the file 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));;
 
-	if (parent == NULL || new == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = value;
