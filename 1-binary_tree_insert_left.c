b35c897c11536527734288ff662ba3ae89d7fe1e
AxelNAY <axelnay@sfr.fr>
2024-04-09 07:33:28 +0000

    Add the condition parent == NULL for the first if of the file 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if (new == NULL)
+	if (parent == NULL || new == NULL)
 		return (NULL);
 
 	new->n = value;
