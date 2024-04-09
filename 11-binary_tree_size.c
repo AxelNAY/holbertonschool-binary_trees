2de491cd8839b97fd220fcb79b88f6f0bdbb889b
AxelNAY <axelnay@sfr.fr>
2024-04-09 13:52:47 +0000

    Update the file 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,11 +11,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	size_t left = binary_tree_size(tree->left);
+	size_t right = binary_tree_size(tree->right);
 
-	size_t left = binary_tree_height(tree->left);
-	size_t right = binary_tree_height(tree->right);
-
-	return (left + right);
+	return (left + right + 1);
 }
