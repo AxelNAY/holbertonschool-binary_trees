0702f29586c5899c7fd8e036ede5d333d72f492c
AxelNAY <axelnay@sfr.fr>
2024-04-09 20:33:52 +0000

    Complete the file 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree.
+ * @tree: a pointer to the root node of the tree to traverse.
+ * Return: the count.
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		count++;
+	if (tree->right != NULL)
+		count++;
+
+	return (count);
+}
