826a910217f517cee8d94adaaa88173719f4a8ae
AxelNAY <axelnay@sfr.fr>
2024-04-10 09:20:27 +0000

    initialise height1 and height2 after the second if

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height1 = binary_tree_height(tree->left);
-        size_t height2 = binary_tree_height(tree->right);
+	size_t height1;
+	size_t height2;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
+
+	height1 = binary_tree_height(tree->left);
+	height2 = binary_tree_height(tree->right);
 
 	if (height1 > height2)
 		return (height1 + 1);
