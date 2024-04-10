cd55d7e75b181a5ee722e429dc775ba91f8a6351
AxelNAY <axelnay@sfr.fr>
2024-04-10 09:16:40 +0000

    put the declaration of height1 and height2 at the start

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height1 = binary_tree_height(tree->left);
+        size_t height2 = binary_tree_height(tree->right);
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-
-	size_t height1 = binary_tree_height(tree->left);
-	size_t height2 = binary_tree_height(tree->right);
 
 	if (height1 > height2)
 		return (height1 + 1);
