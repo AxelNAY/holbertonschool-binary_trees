5cf69b2c76a888438ad11e422a49dd170ee323b2
AxelNAY <axelnay@sfr.fr>
2024-04-10 13:13:38 +0000

    Update the file 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent, *uncle;
 
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	parent = node->parent;
